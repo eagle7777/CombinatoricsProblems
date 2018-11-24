88afddff3f0498b32d2596f385b5547b11c64200
Valentyn <valentyn.kofanov@gmail.com>
2018-11-24 21:20:32 +0200

    create FibRecursiveTopDown.py

---
+++ FibonacciNumbers/FibRecursiveTopDown.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding:utf-8
+# Author: Valentyn Kofanov (knu)
+# source: 
+# Created: 11/24/18
+
+import time
+
+
+def fib(n):
+    """
+    Нахождение n числа Фибоначи
+    :param n: номер
+    :return: искомое число
+    """
+    if n == 1 or n == 2:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+
+if __name__ == '__main__':
+    n = 0
+    while True:
+        n += 1
+        t_start = time.time()
+        print(f'fib({n}) = {fib(n)}')
+        t_end = time.time()
+        if t_end - t_start >= 60:
+            print(f'\n---- n = {n} ----\n')
+            break
