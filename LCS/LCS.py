7d9a11fa8638d2f5253771a7332e35a8dfc448d9
Valentyn <valentyn.kofanov@gmail.com>
2018-11-25 15:02:38 +0200

    update LCS.py

--- LCS/LCS.py
+++ LCS/LCS.py
@@ -5,15 +5,19 @@
 # Created: 11/25/18
 
 
-import time
 from tools import benchmark
 
 
-def LCS(x: str, y:str):
-    ...
+@benchmark
+def LCS(x: str, y: str, showMatrix=False):
+    m = findMatrix(x, y)
+    if showMatrix:
+        for row in m:
+            print(row)
+        print()
+    return max([max(i) for i in m])
 
 
-# @benchmark
 def c(i: int, j: int, x: str, y: str):
     if (i == 0 or j == 0) and x[i] == y[j]:
         return 1
@@ -25,15 +29,17 @@ def c(i: int, j: int, x: str, y: str):
         return max(c(i-1, j, x, y), c(i, j-1, x, y))
 
 
+def findMatrix(x: str, y: str):
+    l1 = len(x)
+    l2 = len(y)
+    m = [[[] for _ in range(l2)] for _ in range(l1)]  # создаем пустую матрицу
+    for i in range(l1):
+        for j in range(l2):
+            m[i][j] = c(i, j, x, y)
+    return m
+
+
 if __name__ == '__main__':
-    t_start = time.time()
-    x = '1123456'
-    y = '123'
-    maxx = -1
-    for i in range(len(x)):
-         for j in range(len(y)):
-             if c(i, j, x, y) > maxx:
-                 maxx = c(i, j, x, y)
-    print(maxx)
-    t_end = time.time()
-print(f'time elapsed : {t_end - t_start}')
+    x = '100234'
+    y = '0124'
+    print(LCS(x, y, showMatrix=True))
