bb8cbec8bc27b955ec0545907b6e0062bd1977c7
Valentyn <valentyn.kofanov@gmail.com>
2018-11-30 23:13:29 +0200

    create CutRodNaive.py

---
+++ CutRodFull/CutRodNaive.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Author: Valentyn Kofanov (knu)
+# Created: 11/29/18
+
+
+"""
+Задача о разрезании отрезка
+"""
+
+import sys
+
+
+def CutRod(p: list, n: int) -> int:
+    """
+    по заданным отрезку и длину стержня находит максимальную прибыль
+    :param p: список стоимости стержней разных длин (индекс - длина, элемент - стоимость)
+    :param n: длина стержня
+    :return:
+    """
+    if n == 0:
+        return 0
+    q = - sys.maxsize
+    for i in range(n):
+        q = max(q, p[i] + CutRod(p, n - i - 1))
+    return q
+
+
+if __name__ == '__main__':
+    p = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+    print(CutRod(p, len(p)))
