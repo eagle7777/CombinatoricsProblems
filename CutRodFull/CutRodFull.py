28a89494c54753327ab6874c1d9f391555095e8d
Valentyn <valentyn.kofanov@gmail.com>
2018-12-01 01:02:06 +0200

    update CutRodFull

--- CutRodFull/CutRodFull.py
+++ CutRodFull/CutRodFull.py
@@ -30,7 +30,7 @@ def CutRodRecursively(p: MyList, n: int) -> int:
     """
     if n == 0:
         return 0
-    q = - sys.maxsize
+    q = - sys.maxsize - 1
     for i in range(n):
         q = max(q, p[i] + CutRodRecursively(p, n - i - 1))  # разбиваем задачи на подзадачи (рекурсивно)
     return q
@@ -40,20 +40,21 @@ def CutRodRecursively(p: MyList, n: int) -> int:
 @benchmark
 def CutRodDynamic(p: MyList, n: int) -> int:
     """
-    по заданным отрезку и длину стержня находит максимальную прибыль
+    по заданным отрезку и длине стержня находит максимальную прибыль
     :param p: список стоимости стержней разных длин (индекс - длина, элемент - стоимость)
     :param n: длина стержня
     :return:
     """
     if n == 0:
         return 0
-    q = - sys.maxsize
+    q = - sys.maxsize - 1
     for i in range(n):
         q = max(q, p[i] + CutRodDynamic(p, n - i - 1))  # разбиваем задачи на подзадачи (рекурсивно)
     return q
 
 
 if __name__ == '__main__':
+    # test
     p = MyList([1, 5, 8, 9, 10, 17, 17, 20, 24, 30])
     print(CutRodRecursively(p, len(p)))
     print(CutRodDynamic(p, len(p)))
---
+++ CutRodFull/ExtendedBottomUp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Author: Valentyn Kofanov (knu)
+# Created: 11/30/18
+
+
+def ExtendedBottomUpCutRod(p: list, n: int, price=0):
+    """
+    по заданным отрезку длине стержня и стоимости
+    находит для каждой длины:
+    максимальную прибыль
+    и список раззрезов для заднной длины
+    :param p: список стоимостей кусков стержня
+    :param n: длина
+    :param price: цена за разрез
+    :return: список макс прибылей и список разрезов
+    """
+    res = [0] * (n + 1)
+    seq = [[]] * (n + 1)
+    for j in range(1, n + 1):
+        q = 0
+        cur_index = 0
+        for i in range(j):
+            if q < p[i] + res[j - i - 1] - price:
+                q = p[i] + res[j - i - 1] - price
+                cur_index = i
+        seq[j] = seq[j - cur_index - 1] + [cur_index]
+        res[j] = q
+    return res, seq
+
+
+if __name__ == '__main__':
+    # test
+    p = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+    print(ExtendedBottomUpCutRod(p, len(p)))
